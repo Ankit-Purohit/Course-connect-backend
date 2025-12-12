ec27afbebfb4e1134a7ff373df73cac3121c0302
ankit.purohit <ankitpurohit040@gmail.com>
2025-12-12 12:24:27 +0530

    fisrt commit

---
+++ AA/src/main/java/com/Course_Connect_Backend/AA/AaApplication.java
@@ -0,0 +1,13 @@
+package com.Course_Connect_Backend.AA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaApplication.class, args);
+	}
+
+}
